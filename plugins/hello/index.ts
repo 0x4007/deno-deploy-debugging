e8e12a49ff80b86e406e159d8825ba5f78ef0785
0x4007 <gpg@pavlovcik.com>
2025-06-19 22:01:29 +0900

    fix: update plugin to be Deno Deploy compatible and fix JSON payload generation

--- plugins/hello/index.ts
+++ plugins/hello/index.ts
@@ -1,1 +1,7 @@
-console.log("Hello Plugin");
+export default {
+  async fetch(request: Request): Promise<Response> {
+    return new Response("Hello from Deno Deploy Plugin!", {
+      headers: { "content-type": "text/plain" },
+    });
+  },
+};
