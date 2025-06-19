327a1a808ab217fecefa63f76c0dde96979f035a
0x4007 <gpg@pavlovcik.com>
2025-06-20 05:31:47 +0900

    refactor: simplify

--- plugins/hello/index.ts
+++
@@ -1,7 +0,0 @@
-export default {
-  async fetch(request: Request): Promise<Response> {
-    return new Response("Hello from Deno Deploy Plugin!", {
-      headers: { "content-type": "text/plain" },
-    });
-  },
-};
